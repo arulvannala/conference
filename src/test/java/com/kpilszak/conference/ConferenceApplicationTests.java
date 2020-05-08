e868a72547fed6a69d45e3e87a364018e4e72a60
kpilszak <klaudia.pilszak@gmail.com>
2020-05-08 13:47:03 +0200

    Create repo

---
+++ src/test/java/com/kpilszak/conference/ConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpilszak.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
